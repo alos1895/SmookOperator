9510d63faece12af57bc19bce631cd193b271151
Alonso Sanchez <alonso.sanchez@lamarina.mx>
2021-03-01 13:31:03 -0600

    Carga de proyecto en blanco

---
+++ SmookOperator/Models/DetailDevice.cs
@@ -0,0 +1,11 @@
+﻿namespace SmookOperator.Models
+{
+    public class DetailDevice
+    {
+        public string device { get; set; }
+        public string hmd { get; set; }
+        public string ppm { get; set; }
+        public string temp { get; set; }
+        public string time { get; set; }
+    }
+}
