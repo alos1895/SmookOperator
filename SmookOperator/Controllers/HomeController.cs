904adb6da0995d4bcc71b71b40f4461c79b53db0
Alonso Sanchez <alonso.sanchez@lamarina.mx>
2021-03-01 18:03:55 -0600

    Se agrega consulta de detalle del dispositivo

--- SmookOperator/Controllers/HomeController.cs
+++ SmookOperator/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using SmookOperator.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace SmookOperator.Controllers
@@ -32,6 +36,38 @@ namespace SmookOperator.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [Route("/detail")]
+        public IActionResult Detalle()
+        {
+            return View();
+        }
+
+        [HttpGet("/detailLastMeasure/{deviceId}")]
+        public async Task<IActionResult> GETDetailDevice(string deviceId)
+        {
+            try
+            {
+                var httpPeticion = (HttpWebRequest)WebRequest.Create($"http://volcani.herokuapp.com/find-one/{deviceId}");
+                httpPeticion.ContentType = "application/json;charset=utf-8";
+                httpPeticion.Method = "GET";
+
+                var httpRespuesta = (HttpWebResponse)httpPeticion.GetResponse();
+                string results;
+
+                var detailDevice = new DetailDevice();
+                using (var lector = new StreamReader(httpRespuesta.GetResponseStream()))
+                {
+                    results = await lector.ReadToEndAsync();
+                }
+                detailDevice = JsonConvert.DeserializeObject<DetailDevice>(results);
+
+                return Ok(detailDevice);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
         }
     }
 }
